99b3753a01a3effe280b8b152e142d4cefec61d4
sofia-papa <papa.sofia@outlook.it>
2021-09-29 17:24:48 +0100

    main container

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AlbumList msg=""/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AlbumList from './components/AlbumList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AlbumList
   }
 }
 </script>
 
 <style lang="scss">
+@import './style/general.scss';
+@import './style/fontandcolor.scss';
+
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  
 }
 </style>
---
+++ src/components/AlbumList.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="bg-color">
+    <div class="logo-container">
+      <div class="row">
+        <div class="col-12">
+          <img src="" alt="">
+        </div>
+      </div>
+    </div>
+
+    <div class="album-wrapper container">
+      <div class="row gy-3">
+        <div v-for= "album in albumList" :key= "album.title" class="col-lg-2 col-md-4 col-6 album me-4">
+          <img class="img-fluid" :src= "album.poster" :alt= "album.title">
+          <h4>{{album.title}}</h4>
+          <p>{{ album.author }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'AlbumList',
+  props: {
+    msg: String
+  },
+  data : function (){
+    return {
+      albumList : []
+    }
+  },
+
+  created: function () {
+    axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+    .then((response)=> {
+      this.albumList = response.data.response.slice();
+      console.log(this.albumList);
+    }
+    )
+  }
+
+}
+
+</script>
+
+
+<style scoped lang="scss">
+@import "../style/fontandcolor.scss";
+@import "../style/general.scss";
+
+.bg-color{
+  background-color: $mainColor;
+
+  .logo-container{
+    height: 80px;
+    width: 100%;
+    background-color: $headerColor;
+  }
+
+.album-wrapper{
+  padding: 5rem 4rem;
+
+  .album{
+      background-color: $headerColor;
+
+      img{
+        padding: 1rem 1rem;
+      }
+  }
+}
+}
+
+
+</style>
